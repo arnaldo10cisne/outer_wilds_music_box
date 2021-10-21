3e920978f6b77ee8a2ada0030b871bece75dfcb1
Arnaldo Cisneros <arnaldo.jose.cisneros.zambrano@gmail.com>
2021-10-21 17:49:03 -0500

    Created working prototype, added silence guide

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ function App() {
       <p>
         Activate and deactivate the instruments of each of the travelers to your liking
       </p>
-      <input type="checkbox" /><label>Banjo</label>
-      <input type="checkbox" /><label>Whistle</label>
-      <input type="checkbox" /><label>Flute</label>
-      <input type="checkbox" /><label>Harmonica</label>
-      <input type="checkbox" /><label>Drums</label>
-      <input type="checkbox" /><label>Piano</label>
-      <input type="checkbox" /><label>Cello</label>
+      <input id="chkBanjo" type="checkbox" /><label>Banjo</label>
+      <input id="chkWhistle" type="checkbox" /><label>Whistle</label>
+      <input id="chkFlute" type="checkbox" /><label>Flute</label>
+      <input id="chkHarmonica" type="checkbox" /><label>Harmonica</label>
+      <input id="chkDrums" type="checkbox" /><label>Drums</label>
+      <input id="chkPiano" type="checkbox" /><label>Piano</label>
+      <input id="chkCello" type="checkbox" /><label>Cello</label>
     </div>
   );
 }
